df2dd040e75e9581918718c1aa33568453795d52
Richard Sneyd <richardsneyd@hotmail.com>
2023-10-09 14:55:07 +0100

    expanded vocab

--- vocab.js
+++ vocab.js
@@ -1,10 +1,9 @@
 const conjunctions = ["but", "and", "then", "for", "if", "when", "after", "before", "because", "so", "or", "also"];
-const adjectives = ["big", "hot", "red", "new", "old", "fat", "bad", "mad", "sad", "green", "purple", "orange", "black", "white", "small", "little", "thin", "dim", "fit", "tall", "wet", "zip", "cool", "warm", "long", "hard", "soft", "fast", "slow", "happy", "rich", "poor", "blue", "wise", "short", "yellow", "old", "young"];
-const nouns = ["cat", "dog", "pig", "man", "bat", "hat", "hen", "cap", "mat", "pen", "car", "box", "hen", "sun", "ball", "girl", "boy", "mom", "dad", "moon", "star", "bird", "fish", "milk", "food", "zip", "tree", "leaf", "ship", "ring", "bell", "boat", "root", "door", "chair", "table", "key", "horse", "monkey", "chicken", "house"];
-const verbs = ["run", "sit", "hop", "dig", "fix", "win", "cut", "hit", "mix", "cry", "see", "fly", "lie", "buy", "pay", "say", "eat", "sip", "read", "ride", "open", "shut", "sing", "jump", "talk", "walk", "swim", "take", "pop", "pet", "crawl", "pat", "fall", "drink", "sing", "dance", "laugh"];
-const adverbs = ["now", "too", "low", "high", "near", "far", "well", "fast", "hard", "then", "here", "soon", "late", "very", "just", "also", "early", "there", "quite", "often", "never", "seldom", "almost", "always"];
-const spatialPrepositions = ["up", "down", "left", "right", "beside", "behind", "on", "under", "where"];
-// const questionWords = ["why", "when", "where", "how", "who", "do", "does", "did"]; 
+const adjectives = ["big", "hot", "red", "wet", "new", "old", "fat", "bad", "mad", "sad", "fun", "bald", "hard", "soft", "green", "dry", "purple", "orange", "black", "white", "small", "raw", "little", "thin", "dim", "fit", "tall", "wet", "zip", "cool", "warm", "long", "hard", "soft", "fast", "slow", "happy", "rich", "poor", "blue", "wise", "short", "yellow", "old", "young"];
+const nouns = ["cat", "dog", "pig", "man", "rat", "bat", "hat", "hen", "cap", "mat", "pen", "car", "box", "hen", "sun", "pot", "jar", "log", "bed", "pan", "mug", "bug", "lip", "lap", "lad", "bag", "bed", "pan", "ball", "girl", "boy", "mom", "dad", "moon", "star", "bird", "fish", "milk", "food", "zip", "tree", "seed", "leaf", "ship", "ring", "bell", "boat", "root", "door", "chair", "table", "key", "horse", "monkey", "chicken", "house"];
+const verbs = ["run", "sit", "hop", "dig", "fix", "win", "cut", "hit", "nod", "tap", "tug", "saw", "mix", "dig", "cry", "see", "fly", "lie", "buy", "pay", "say", "eat", "sip", "read", "ride", "open", "shut", "sing", "jump", "talk", "walk", "swim", "take", "pop", "pet", "crawl", "pat", "fall", "drink", "sing", "dance", "laugh"];
+const adverbs = ["now", "yet", "too", "low", "high", "near", "far", "well", "fast", "hard", "then", "here", "soon", "late", "very", "just", "also", "early", "there", "quite", "often", "never", "seldom", "almost", "always"];
+const spatialPrepositions = ["up", "down", "on", "under", "left", "right", "beside", "behind", "where"];
 const questionWords = ["when", "how"]; 
 
 
